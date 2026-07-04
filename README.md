fc3c09cd0c58d319d6968235f18e682e76174486
nstrucky <nickstruckmeyer@outlook.com>
2026-07-04 14:52:25 -0400

    lab instructions and init of model and tokenizer

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # My Chatbot
 
 Simple chatbot project to test LLM capabilities
+
+## Setup
+
+Use the setup.sh to install python virtual environment and dependecies within
--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,11 @@
 
 from transormers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+model_name = "facebook/blenderbot-400M-distill"
+
+# Load model (download on first run and reference local installation for subsequent runs)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+# Load tokenizer which optimizes input and passes it to the model efficiently
+# Meaning it turns input into tokens
+tokenizer = AutoTokenizer.from_pretrained(model_name)
